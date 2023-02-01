3c29651e7284c1ef928ad00f17f8a2eae1a01759
Anton <anton.source@gmail.com>
2023-02-01 20:16:34 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+TimeSpan startTime = new TimeSpan(0,0,0);
+run(startTime);
+
+TimeSpan run(TimeSpan initTime) {
+    var sw = new Stopwatch();
+    sw.Start();
+    TimeSpan ts = new TimeSpan(0,0,0);
+    ConsoleKeyInfo key = new ConsoleKeyInfo();
+    while(true) {
+        if (Console.KeyAvailable) {
+            key = Console.ReadKey(true);
+            Console.WriteLine("\nWas pressed " + key.Key);
+
+            if (key.Key == ConsoleKey.Escape) {
+                sw.Stop();
+                break;
+            }
+            else if (key.Key == ConsoleKey.Spacebar) {
+                Console.WriteLine("Press Spacebar for continue\n" +
+                "Esc for exit\n" +
+                "Z for zeroing");
+                ts = sw.Elapsed + initTime;
+                key = Console.ReadKey(true);
+                if (key.Key == ConsoleKey.Spacebar) {
+                    run(ts);
+                    break;
+                }
+                else if (key.Key == ConsoleKey.Z) {
+                    run(new TimeSpan(0,0,0));
+                    break;
+                }
+            }
+        }
+        Thread.Sleep(1000);
+        ts = sw.Elapsed + initTime;
+        Console.Write($"\r{ts.Hours}h {ts.Minutes}m {ts.Seconds}s");
+    }
+    sw.Stop();
+    return ts;
+}
